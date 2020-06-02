622043fe6881ba98d45d776c5ff4f237481e5ede
Mark Skelton <mdskelton99@gmail.com>
2020-06-01 20:25:33 -0500

    fix: Add Jest globals to mocks

--- jest.js
+++ jest.js
@@ -1,12 +1,14 @@
 module.exports = {
   overrides: [
     {
-      env: {
-        jest: true,
-      },
+      env: { jest: true },
       extends: ["plugin:jest/recommended", "plugin:jest/style"],
       files: ["**/__tests__/**", "*.{spec,test}.{js,jsx,ts,tsx}"],
       plugins: ["jest"],
+    },
+    {
+      env: { jest: true },
+      files: ["**/__mocks__/**"],
     },
   ],
 };
