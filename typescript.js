e659eb610af34e036d90d7a00eb6b296fa1e0257
Mark Skelton <mdskelton99@gmail.com>
2020-06-03 21:51:24 -0500

    fix: Turn off @typescript-eslint/no-empty-function rule

--- typescript.js
+++ typescript.js
@@ -10,6 +10,7 @@ module.exports = {
       rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-useless-constructor": "error",
         "@typescript-eslint/no-unused-vars": [
